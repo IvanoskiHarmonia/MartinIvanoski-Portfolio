37f5be4680ffeb6741408c75d8aea258e57f0bcb
Martin Ivanoski <martinivanoski@Martins-MBP.lan>
2023-08-24 20:34:49 -0500

    MIPO-2: fixed centering home, new stars animation, removed background per section, CSS refactoring

--- app/js/script.js
+++ app/js/script.js
@@ -1,6 +1,22 @@
-var prevScrollpos = window.pageYOffset;
+var prevScrollpos = window.scrollY;
+
 window.onscroll = function() {
-var currentScrollPos = window.pageYOffset;
+  navbarAnimation();
+  starParallax();
+}
+
+function starParallax() {
+  var scrolled = window.scrollY;
+  var stars = document.getElementById('background-stars');
+
+  var xPosition = scrolled * 0 + 'px';
+  var yPosition = scrolled * 1 + 'px';
+
+  stars.style.backgroundPosition = xPosition + ' ' + yPosition + ', ' + xPosition + ' ' + yPosition + ', ' + xPosition + ' ' + yPosition + ', ' + xPosition + ' ' + yPosition;
+}
+
+function navbarAnimation() {
+  var currentScrollPos = window.scrollY;
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("navbar").style.top = "0";
   } else {
@@ -8,7 +24,7 @@ var currentScrollPos = window.pageYOffset;
   }
   prevScrollpos = currentScrollPos;
 }
-
+  
 function redirect(link) {
   window.open(link, '_blank');
 }
